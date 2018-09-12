b9f8d69f42b3aceb9739f1f5a0f39298ba169603
github username <github email address>
2018-09-12 17:08:04 -0400

    action cable beginning to ping circles

--- app/serializers/paint_room_serializer.rb
+++ app/serializers/paint_room_serializer.rb
@@ -1,6 +1,7 @@
 class PaintRoomSerializer < ActiveModel::Serializer
     attributes :name
-    has_many :users, serializer: UserSerializer
-    has_many :circles, serializer: CircleSerializer
-    
+    # has_many :users, serializer: UserSerializer
+    # has_many :circles, serializer: CircleSerializer
+
+
 end
