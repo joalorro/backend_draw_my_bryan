c61347cd595f418f4cb75b38153e92ceff015a4b
github username <github email address>
2018-09-13 11:19:35 -0400

    fixed backend for circles

--- app/channels/circles_channel.rb
+++ app/channels/circles_channel.rb
@@ -1,10 +1,25 @@
 class CirclesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "circles_channel"
-    puts "sup"
+    CirclesChannel.all_circles(Circle.all)
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def send_circle data
+    Circle.create(x: data['x'], y: data['y'] , strokeWidth: data['strokeWidth'] ,strokeColor: data['strokeColor'])
+    ActionCable.server.broadcast('circles_channel',
+      x: data['x'],
+      y: data['y'],
+      strokeWidth: data['strokeWidth'],
+      strokeColor: data['strokeColor']
+    )
+
+  end
+
+  def self.all_circles circles
+    ActionCable.server.broadcast('circles_channel', history:circles)
   end
 end
