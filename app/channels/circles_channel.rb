6c3e241ba130d5f8a7d50c531217e3ed36c0c8da
joalorro <joalorro@gmail.com>
2018-09-13 18:02:56 -0400

    not much changed

--- app/channels/circles_channel.rb
+++ app/channels/circles_channel.rb
@@ -1,18 +1,11 @@
 class CirclesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "circles_channel"
-<<<<<<< HEAD
-    puts "sup"
-=======
     CirclesChannel.all_circles(Circle.all)
->>>>>>> implenting_action_cables
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
-<<<<<<< HEAD
-=======
 
   def send_circle data
     puts "receiving"
@@ -24,11 +17,9 @@ class CirclesChannel < ApplicationCable::Channel
       strokeWidth: data['strokeWidth'],
       strokeColor: data['strokeColor']
     )
-
   end
 
   def self.all_circles circles
     ActionCable.server.broadcast('circles_channel', history:circles)
   end
->>>>>>> implenting_action_cables
 end
